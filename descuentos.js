b72cff27a60df743c46f711fac514d9d4677acda
santosmc93 <carlos.santosm93@gmail.com>
2021-11-26 18:06:19 -0600

    Se añaden archivos de la segunda practica de JS practico

---
+++ descuentos.js
@@ -0,0 +1,46 @@
+const precioOriginal = 100;
+const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio*porcentajePrecioConDescuento)/100;
+    return precioConDescuento
+}
+
+ function ButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+
+    valuePrice = inputPrice.value;
+    valueDiscount = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(valuePrice,valueDiscount);
+
+    const resultadoPrecio = document.getElementById("Resultado")
+    resultadoPrecio.innerText = "El precio con desuento son: $" + precioConDescuento;
+}
+
+const coupons = [
+    {
+        name: "BlackFriday",
+        discount: 15
+    },
+    {
+        name: "JavaScript",
+        discount: 30
+    },
+    {
+        name: "Web",
+        discount: 25
+    }
+]
+
+
+
+
+
+//  console.log({
+//     precioOriginal,
+//     descuento,
+//     precioConDescuento
+// })
